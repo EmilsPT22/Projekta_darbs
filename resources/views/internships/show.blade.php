593710d5acd18026ada2bab044b1749199646943
Emils <emils@gmail.com>
2025-12-01 12:02:32 +0200

    pielaboju show blade

--- resources/views/internships/show.blade.php
+++ resources/views/internships/show.blade.php
@@ -12,6 +12,45 @@
         <li class="list-group-item"><strong>End Date:</strong> {{ \Carbon\Carbon::parse($internship->end_date)->format('d/m/Y') }}</li>
     </ul>
 
+    <h2 class="mb-3">Added Students</h2>
+    @if($addedStudents->isEmpty())
+        <p>No students added yet.</p>
+    @else
+        <ul class="list-group mb-4">
+            @foreach($addedStudents as $student)
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    {{ $student->name }} ({{ $student->email }})
+                    <form action="{{ route('internships.removeStudent', ['internship' => $internship->id, 'id' => $student->id]) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger btn-sm">
+                            Remove
+                        </button>
+                    </form>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+
+    <h2 class="mb-3">Add Students</h2>
+    @if($users->isEmpty())
+        <p>All students have been added.</p>
+    @else
+        <ul class="list-group mb-4">
+            @foreach($users as $user)
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    {{ $user->name }} ({{ $user->email }})
+                    <form action="{{ route('internships.addStudent', ['internship' => $internship->id, 'id' => $user->id]) }}" method="POST">
+                        @csrf
+                        <button type="submit" class="btn btn-success btn-sm">
+                            Add
+                        </button>
+                    </form>
+                </li>
+            @endforeach
+        </ul>
+    @endif
+
     <a href="{{ route('internships.index') }}" class="btn btn-primary">Back to Internships</a>
 </div>
 @endsection
